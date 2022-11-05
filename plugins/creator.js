6435b7180f4fb1d99e8351198e5c8819b5e9ec9a
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-11-05 11:53:55 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ function handler(m) {
   
   const kontak = {
 	"displayName": 'My owner',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('6283820073017@s.whatsapp.net')}\nitem1.TEL;waid=6283820073017:6283820073017\nitem1.X-ABLabel:Busy.\nURL;My Web: https://hi.rlxfly.my.id\nEMAIL;Email Owner: me@rlxfly.my.id\nORG: NOT A BOT + NO SAVE\nTEL;My number bot;waid=6287845646738:6287845646738\nEND:VCARD`
+	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('60102810046@s.whatsapp.net')}\nitem1.TEL;waid=60102810046:60102810046\nitem1.X-ABLabel:Busy.\nURL;My Web: https://github.com/ai-man-123\nEMAIL;Email Owner: lynxaiman@yahoo.com\nORG: NOT A BOT + NO SAVE\nTEL;My number bot;waid=60102810046:60102810046\nEND:VCARD`
 }
 
 conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
